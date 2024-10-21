01d4483d59df65f9da4fd0f3ce11455c1e88c294
HiLordReilo <dreamerlightsun@outlook.com>
2024-10-22 01:51:38 +0300

    Fix incorrect locale name on older systems

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,13 @@ namespace BST_SheetsEditor
     /// </summary>
     public partial class App : Application
     {
+        // Apparently Windows 8.1 (and maybe earlier versions as well) uses "ja-JP" for Japanese locale name
+        // while Windows 10 uses "jp-JP"
+        // no clue about Windows 11, I don't use this piece of shit
+        // neither about Windows 7. OS is fine, but I grew up with 8
+        //
+        // but I digress
+        public static string LocaleName = Environment.OSVersion.Version.Major < 10 ? "ja-JP" : "jp-JP";
     }
 
 }
